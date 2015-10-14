57e752bcfe7b6ef796740d155b11518caf5f8a20
luongbangnguyen <luongbangvh@gmail.com>
2015-10-14 10:11:13 +0700

    Init project

---
+++ src/main/java/com/java/spring/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.java.spring;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ReactSpringDataRestApplication.class);
+	}
+
+}
